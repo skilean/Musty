da4509135bd002c2295c80f8e7304e5edb8b41b7
Art <obscurexdlol@gmail.com>
2017-11-28 20:56:53 +0300

    Добавлены элементы управления в CreateEditActivity добавлен переход на CreateEditActivity

--- app/src/main/java/com/android/musty/MainActivity.java
+++ app/src/main/java/com/android/musty/MainActivity.java
@@ -1,13 +1,28 @@
 package com.android.musty;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button CreateEditButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CreateEditButton = findViewById(R.id.add_button);
+
+        CreateEditButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, CreateEditActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
