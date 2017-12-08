7e1b521df3e8eb5034c2676617956b284b723612
DESKTOP-UFSB750\SomgBird <dlarionov51@gmail.com>
2017-12-08 15:28:30 +0300

    Доработан класс DBHelper. Реализован класс TableInteraction.

--- app/src/main/java/com/android/musty/DBHelper.java
+++ app/src/main/java/com/android/musty/DBHelper.java
@@ -3,23 +3,50 @@ package com.android.musty;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 import android.util.Log;
 
+
 public class DBHelper extends SQLiteOpenHelper {
-    public DBHelper(Context context) {
-        super(context, "DB", null, 1);
+    private static final int db_version = 1;
+
+    // Класс содержит данные о таблице заметок и некоторый свойства класса
+    public static abstract class Columns implements BaseColumns {
+        public static final String TABLE_NAME = "notes", // Название таблицы для хранения заметок
+                NOTE_ID = "note_id", // Идентификатор заметки
+                NOTE_HEADER = "note_header", // Заголовок/название заметки
+                NOTE_TAGS = "note_tags", // Строка с тегами, разделенными пробелами
+                NOTE_DATE = "note_date", // Назначенная дата
+                NOTE_CATEGORY = "note_category", // Категория заметки
+                NOTE_MEDIA = "note_media"; // Ссылка на информацию из медиасервиса
     }
 
+    // SQL-запрос для создания базы данных
+    private final String create_table = "create table if not exists" + Columns.TABLE_NAME + "("
+            + Columns.NOTE_ID + "integer primary key autoincrement,"
+            + Columns.NOTE_HEADER + "text,"
+            + Columns.NOTE_TAGS + "text,"
+            + Columns.NOTE_DATE + "text,"
+            + Columns.NOTE_CATEGORY + "text"
+            + Columns.NOTE_MEDIA + "text" + ");";
+
+    /**
+     * Конструктор.
+     * @param context контекст вызова
+     */
+    public DBHelper(Context context) {
+        super(context, "DB", null, db_version);
+    }
+
+    /**
+     * Создает базу данных
+     * @param db объект SQLiteDatabase для запроса к базе данных
+     */
     @Override
     public void onCreate(SQLiteDatabase db){
-        Log.d("log","--- onCreate databse ---");
+        Log.d("db","Создана база банных.");
 
-        db.execSQL("create table notes ("
-                + "note_id integer primary key autoincrement,"
-                + "note_header text,"
-                + "note_tags text,"
-                + "note_date text,"
-                + "note_category text" + ");");
+        db.execSQL(create_table);
     }
 
     @Override
